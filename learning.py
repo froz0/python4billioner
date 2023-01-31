94051ec791eb49c4b43f9e120e2bcafeaf4a5b77
froz <91838924+froz0@users.noreply.github.com>
2023-01-31 09:42:06 +0300

    Update learning.py

--- learning.py
+++ learning.py
@@ -1,5 +1,20 @@
+# #Python Syntax
+# print("hello world") #this an python syntax
 
+# #Python Indentation
+# if 5 < 6:
+#   print("five is less than six!")
 
-if 4<6:
+# if 5 > 2:
+# print("Five is greater than two!") #this an error
 
- print:("four smaller than six!")
+# if 6 > 4:
+#       print('six is greater than four')#this is an correct
+
+# if 5 < 9:
+#   print("five is less than nine")
+#      print("five is less than nine") #this is an error
+
+# if 5 < 9:
+#   print("five is less than nine")
+#   print("five is less than nine") # this is  correct
