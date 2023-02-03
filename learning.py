8f9fd6cb23e60428a5b4fc72d4675fd1e87c4579
froz <91838924+froz0@users.noreply.github.com>
2023-02-03 10:41:59 +0300

    Day 8

--- learning.py
+++ learning.py
@@ -18,3 +18,48 @@
 # if 5 < 9:
 #   print("five is less than nine")
 #   print("five is less than nine") # this is  correct
+
+# 03/02/23
+
+# Variable Names
+
+# myvar = "froz"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+# print(myvar, _my_var) #this is a variable names
+
+# QowlE = "manga"
+# al_bashiiri = "novel"
+# _fr0_z = "anime"
+# SALIH = 'GOT'
+# dost = [SALIH, QowlE, al_bashiiri, _fr0_z]
+# print(dost) #this is a variable names
+
+#  2qowle = 'froz'
+# qow-le = 'albashiiri'
+# qow le = "salih" #this is an error
+
+# 2elma = "water"
+# ekme-k = "yamurta"
+# tat li = "sigaret" #this is an error
+
+# Many Values to Multiple Variables
+# Y = x = W = "Kedi"
+# print(Y)
+# print(x)
+# print(W) # this is a Many Values to Multiple Variables
+
+# froz = ["qowle", "albashiiri", "salih"]
+# x, h, f = froz
+# print(x, h, f) #Unpack a Collection
+
+# yahya = ["solo leveling", "manga", "bleach"]
+# yahya = x, y, z
+# print(x, y, z) # this is error
+
+# m3 = ['sara', 3, 'mia khalifa']
+# d3, s3, c8 = m3
+# print(d3) #this is froz's project.
